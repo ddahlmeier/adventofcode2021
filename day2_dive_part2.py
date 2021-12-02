ea3d7ab83c8f65f96bdd3d247a2c61dbbe9442f7
Daniel Dahlmeier <d.dahlmeier@sap.com>
2021-12-02 21:39:59 +0800

    day 2 part simple solution

---
+++ day2_dive_part2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+"""Execute a series of navigation commands. Determine final position. """
+
+
+import fileinput
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: day2_dive_part2.py <command file>")
+        sys.exit(-1)
+    input_file = sys.argv[1]
+    # position = (horizontal, depth, aim)
+    position = [0, 0, 0]
+    for line in fileinput.input(files=input_file):
+    	if len(line.split()) != 2:
+    		continue
+    	command, value = line.split()
+    	value = int(value)
+    	if command == "forward":
+    		position[0] += value
+    		position[1] += position[2]*value
+    	elif command == "up":
+    		position[2] -= value
+    	elif command == "down":
+    		position[2] += value
+    	else:
+    		print("Unknown command {}", command)
+    print(position[0] * position[1])
