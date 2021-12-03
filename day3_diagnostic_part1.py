8e86cf81cf8a582b0d48d4feba4205a0c995a532
Daniel Dahlmeier <d.dahlmeier@sap.com>
2021-12-03 14:19:41 +0800

    day 3 part 1

---
+++ day3_diagnostic_part1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+
+"""Execute a series of navigation commands. Determine final position. """
+
+import fileinput
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: day3_diagnostic_part1.py <input file>")
+        sys.exit(-1)
+    report = [list(map(int, line.strip())) for line in fileinput.input(files=sys.argv[1])]
+    pivot = list(zip(*report))
+    gamma_rate = int("".join(['1' if one_count > len(report)/2 else '0' for one_count in map(sum, pivot)]), 2)
+    epsilon_rate = int("".join(['0' if one_count > len(report)/2 else '1' for one_count in map(sum, pivot)]), 2)
+    print(gamma_rate*epsilon_rate)
